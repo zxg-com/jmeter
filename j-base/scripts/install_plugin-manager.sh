dfaff301fa044919a0af583d1bf318dc16cb4d0c
zxg-com <342509492@qq.com>
2021-07-12 11:11:59 +0800

    jars

--- j-base/scripts/install_plugin-manager.sh
+++ j-base/scripts/install_plugin-manager.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-CMD_RUNNER_URL="http://search.maven.org/remotecontent?filepath=kg/apc/cmdrunner/2.0/cmdrunner-2.0.jar"
+CMD_RUNNER_URL="http://search.maven.org/remotecontent?filepath=kg/apc/cmdrunner/2.2/cmdrunner-2.2.jar"
 PLUGIN_MANAGER_URL="https://jmeter-plugins.org/get/"
 
 # 下载cmdrunner.jar
-wget -O "lib/cmdrunner-2.0.jar" $CMD_RUNNER_URL
+wget -O "lib/cmdrunner-2.2.jar" $CMD_RUNNER_URL
 
 # 下载插件manager
 wget -O "lib/ext/jmeter-plugins-manager.jar" $PLUGIN_MANAGER_URL
